e5683356c4a0910152a9419db52ba048c1d24597
TR-Wang <79711880+TR-Wang@users.noreply.github.com>
2023-02-03 15:46:38 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Tongrui Wang',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '',
 
   // social usernames (optional)
   twitter: '',
